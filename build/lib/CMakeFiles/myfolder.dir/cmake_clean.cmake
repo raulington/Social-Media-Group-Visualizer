3101b4f0ce0bf11c9864fa60e9a299e63f8b7f7e
Shashank Kambhatla <sk76@illinois.edu>
2022-11-03 23:01:10 +0000

    first commit

---
+++ build/lib/CMakeFiles/myfolder.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myfolder.dir/myfolder/myfolderfile.cpp.o"
+  "CMakeFiles/myfolder.dir/myfolder/myfolderfile.cpp.o.d"
+  "libmyfolder.a"
+  "libmyfolder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myfolder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/lib/CMakeFiles/myfolder.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for myfolder.
+# This may be replaced when dependencies are built.
