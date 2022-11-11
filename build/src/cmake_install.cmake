9813ebf6093dae30b4adfae474a33cf11e662d44
Shashank Kambhatla <sk76@illinois.edu>
2022-11-11 23:44:34 +0000

    input file as map

--- build/src/cmake_install.cmake
+++ build/src/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /workspaces/CS225/cs225_project/src
+# Install script for directory: /workspaces/CS225/Social-Media-Group-Visualizer/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
