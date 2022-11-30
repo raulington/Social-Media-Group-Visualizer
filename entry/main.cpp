bdf68411d55e779686e0ff888a58f3f15a9d3e3f
Raul Daniel Higareda <raul.d.higareda@gmail.com>
2022-11-30 18:31:35 +0000

    Use functions in main file

--- entry/main.cpp
+++ entry/main.cpp
@@ -3,19 +3,45 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include "Between.h"
+#include "GraphTraversal.h"
 
 int main() {
     std::string tmp2;
     std::string tmp3;
     std::string tmp4;
-    std::unordered_map<std::string, std::vector<std::string>> tmp;
+    std::unordered_map<std::string, std::vector<std::string>> map;
     std::ifstream file("facebook_combined.txt");
     while (getline(file, tmp2)) {
         tmp3 = tmp2.substr(0, tmp2.find(" "));
         tmp4 = tmp2.substr(tmp2.find(" "), tmp2.size());
-        tmp[tmp3].push_back(tmp4);
-        tmp[tmp4].push_back(tmp3);
+        map[tmp3].push_back(tmp4);
+        map[tmp4].push_back(tmp3);
+    }
+
+    // Grab most Influential person
+    Between b(map);
+    std::unordered_map<std::string, double> centralities;
+    int maxcentrality = 0;
+    std::string most_influential;
+    for (const auto& pair : centralities) {
+        if (pair.second > maxcentrality) {
+            most_influential = pair.first;
+            maxcentrality = pair.second;
+        }
+    }
+
+    std::cout << "Most influential person: " << most_influential << std::endl;
+
+    // Grab random person
+    int random_idx = rand() % map.size();
+    int count = 0;
+    std::string random_person;
+    for (const auto& pair : map) {
+        if (count == random_idx) {
+            random_person = pair.first;
+            break;
+        }
     }
     file.close();
-
 }
