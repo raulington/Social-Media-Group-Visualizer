393bcfea34adf7f60343173fa4ad2326d87875d2
mukujayCS <80647230+mukujayCS@users.noreply.github.com>
2022-12-10 13:40:52 -0600

    Delete build directory

--- build/lib/CMakeFiles/myfolder.dir/myfolder/myfolderfile.cpp.o.d
+++
@@ -1,2 +0,0 @@
-lib/CMakeFiles/myfolder.dir/myfolder/myfolderfile.cpp.o: \
-  /workspaces/mukundiyengar/CS\ 225/Social-Media-Group-Visualizer/lib/myfolder/myfolderfile.cpp
--- build/test_include-b12d07c.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/workspaces/mukundiyengar/CS 225/Social-Media-Group-Visualizer/build/test_tests-b12d07c.cmake")
-  include("/workspaces/mukundiyengar/CS 225/Social-Media-Group-Visualizer/build/test_tests-b12d07c.cmake")
-else()
-  add_test(test_NOT_BUILT-b12d07c test_NOT_BUILT-b12d07c)
-endif()
